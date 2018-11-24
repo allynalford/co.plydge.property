cbb729defab93ff1967109335ba40ebbde7824cf
anton troyanov <anton@troyanov.net>
2018-11-24 05:14:54 +0300

    provide commit_sha info via docker build-arg

--- update.sh
+++ update.sh
@@ -2,11 +2,10 @@
 echo "pulling latest changes from master"
 git pull origin master
 echo "building new docker image"
-docker build -t co.plydge.property .
+docker build --build-arg COMMIT_SHA=$(git rev-parse --short HEAD) -t co.plydge.property .
 echo "stopping old container"
 docker stop co.plydge.property
 echo "removing old container"
 docker rm co.plydge.property
 echo "starting new container"
 docker run --name co.plydge.property --publish 80:80 -d co.plydge.property
-
