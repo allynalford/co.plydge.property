c35ff0bc1f90c58df13c7b8479cf502617a1b0d3
Allyn Alford <allynalford@gmail.com>
2018-11-27 16:53:18 -0500

    property claim

--- vendor/app/controller/property.go
+++ vendor/app/controller/property.go
@@ -2,10 +2,8 @@ package controller
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"app/model"
 	"app/shared/session"
 	"app/shared/view"
 
@@ -51,7 +49,7 @@ func PropertyCreatePOST(w http.ResponseWriter, r *http.Request) {
 	sess := session.Instance(r)
 
 	// Validate with required fields
-	if validate, missingField := view.Validate(r, []string{"note"}); !validate {
+	if validate, missingField := view.Validate(r, []string{"house_number", "street_direction", "street_name"}); !validate {
 		sess.AddFlash(view.Flash{"Field missing: " + missingField, view.FlashError})
 		sess.Save(r, w)
 		NotepadCreateGET(w, r)
@@ -59,24 +57,34 @@ func PropertyCreatePOST(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Get form values
-	content := r.FormValue("note")
+	houseNumber := r.FormValue("house_number")
 
 	userID := fmt.Sprintf("%s", sess.Values["id"])
 
 	// Get database result
-	err := model.NoteCreate(content, userID)
+	//err := model.NoteCreate(content, userID)
 	// Will only error if there is a problem with the query
-	if err != nil {
-		log.Println(err)
-		sess.AddFlash(view.Flash{"An error occurred on the server. Please try again later.", view.FlashError})
-		sess.Save(r, w)
-	} else {
-		sess.AddFlash(view.Flash{"Note added!", view.FlashSuccess})
-		sess.Save(r, w)
-		http.Redirect(w, r, "/notepad", http.StatusFound)
-		return
-	}
+	//if err != nil {
+	//	log.Println(err)
+	//	sess.AddFlash(view.Flash{"An error occurred on the server. Please try again later.", view.FlashError})
+	//	sess.Save(r, w)
+	//} else {
+	//	sess.AddFlash(view.Flash{"Note added!", view.FlashSuccess})
+	//	sess.Save(r, w)
+	//	http.Redirect(w, r, "/notepad", http.StatusFound)
+	//	return
+	//}
+
+	sess.AddFlash(view.Flash{"Property: " + houseNumber, view.FlashSuccess})
+	sess.Save(r, w)
+
+	// Display the view
+	v := view.New(r)
+	v.Name = "property/claim"
+	v.Vars["houseNumber"] = houseNumber
+	v.Vars["token"] = csrfbanana.Token(w, r, sess)
+	v.Render(w)
 
 	// Display the same page
-	PropertyReadGET(w, r)
+	//PropertyReadGET(w, r)
 }
